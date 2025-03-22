5fe1e651c0b7bfa7606b8da7ff935d1d8ed2352b
baptiste amice <amice.bap@gmail.com>
2025-03-22 19:22:36 +0100

    finish game timeout

--- Game/game.gd
+++ Game/game.gd
@@ -3,6 +3,8 @@ class_name Game
 
 @export var nb_players: int = 2
 @onready var players_h_box_container: HBoxContainer = $Control/PlayersHBoxContainer
+@onready var timer_label: RichTextLabel = $Control/TimerLabel
+@onready var game_timer: Timer = $GameTimer
 
 func _init_params(params: Dictionary) -> void:
 	if params.has("nb_player"):
@@ -12,3 +14,13 @@ func _ready() -> void:
 	var soundtracks = Global.GAME_CONTROLLER.soundtracks.get_children()
 	soundtracks[0].stop()
 	soundtracks[1].play()
+	
+	timer_label.text = str(int(game_timer.time_left))
+
+
+func _on_game_timer_timeout() -> void:
+	pass # Replace with function body.
+
+
+func _on_seconds_timer_timeout() -> void:
+	timer_label.text = str(int(game_timer.time_left))
