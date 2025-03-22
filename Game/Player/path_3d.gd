be1f8a1aed56c94efc1c33fb36e1bd4a06b17051
baptiste amice <amice.bap@gmail.com>
2025-03-22 12:15:15 +0100

    key labels in game

--- Game/Player/path_3d.gd
+++ Game/Player/path_3d.gd
@@ -1,10 +1,41 @@
 extends Path3D
 
 @onready var path_follow_3d: PathFollow3D = $PathFollow3D
+@export var progress_speed: float = 2  # Speed of the path following, adjusted based on delta time
+@onready var bread_list: Node3D = $"../BreadList"
+@onready var player: Player = $".."
 
 func _ready() -> void:
 	path_follow_3d.progress = 1
 
-func _on_timer_timeout() -> void:
-	path_follow_3d.progress += 0.01  # Move the PathFollow3D every 2 seconds
-	print("aaa")
+func _physics_process(delta: float) -> void:
+	# Update progress based on delta time to ensure consistent movement regardless of frame rate
+	path_follow_3d.progress += progress_speed * delta
+	
+	if Input.is_action_just_pressed(player.key_drop):
+		spawn_bread()
+	if Input.is_action_just_pressed(player.key_power):
+		print("I HAVE DA POWER")
+	
+
+	
+func duplicate_at_follow_point(node: Node3D, new_parent: Node):
+	# Duplicate the node
+	var new_object = node.duplicate()
+
+	# Ensure the new object is inside the tree before setting global properties
+	new_parent.add_child(new_object)
+
+	# Now that it's added to the tree, set the local transform
+	new_object.global_transform.origin = node.global_transform.origin
+	new_object.scale = node.scale
+
+	
+func spawn_bread():
+	if path_follow_3d.get_child_count() > 0:
+		var first_child = path_follow_3d.get_child(0)
+		duplicate_at_follow_point(first_child, bread_list)
+	else:
+		print("No children found.")
+	
+	
--- Game/Player/player.gd
+++ Game/Player/player.gd
@@ -1,1 +1,12 @@
 extends Node3D
+class_name Player
+
+@export var key_drop: String = "enter"
+@export var key_power: String = "A"
+@onready var power_up_key_label: RichTextLabel = $CanvasLayer/Interface/PowerUpTextureRect/PowerUpKeyLabel
+@onready var drop_key_label: RichTextLabel = $CanvasLayer/Interface/DropKeyLabel
+
+
+func _ready(): 
+	drop_key_label.text = key_drop + " to drop"
+	power_up_key_label.text = key_power
